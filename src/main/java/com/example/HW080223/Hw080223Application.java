dd8fbb4ddb86f49f7ce96986991457e24c53b5fb
Tkakishak <tkakishakd@mail.ru>
2023-08-04 21:38:07 +0300

    startHW

---
+++ src/main/java/com/example/HW080223/Hw080223Application.java
@@ -0,0 +1,13 @@
+package com.example.HW080223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw080223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw080223Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HW080223/Hw080223ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HW080223;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw080223ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
